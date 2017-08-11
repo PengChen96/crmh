622a2862ef7206726b3a59cad06d44fc39612e85
pengchen <2601349541@qq.com>
2017-08-11 18:01:12 +0800

    FlatList

---
+++ app/common/autoTextInput.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {AppRegistry, TextInput, StyleSheet} from 'react-native';
+
+
+export default class AutoExpandingTextInput extends Component {
+    // 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            text: '',
+            lines: 2
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(event) {
+        console.log(event.nativeEvent);
+        let height = event.nativeEvent.contentSize.height;
+        lines = parseInt((height-39)/16.5)+1;
+        console.log(lines);
+        if(lines<=2){
+        	lines = 2;
+        }
+        if(lines>6){
+        	lines = 6;
+        }
+        this.setState({
+            text: event.nativeEvent.text,
+            lines:lines
+        });
+    }
+    
+    render() {
+        return (
+            <TextInput {...this.props}  //将组件定义的属性交给TextInput
+                multiline={true}
+                editable = {true}
+        				maxLength = {4000}
+        				numberOfLines = {this.state.lines}
+                onChange={this.onChange}
+                value={this.state.text}
+            />
+        );
+    }
+}
--- app/ui/msg/room.js
+++ app/ui/msg/room.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import { StyleSheet,Text,View,TextInput,Button,Image } from 'react-native';
+import { StyleSheet,Text,View,TextInput,Button,Image,FlatList } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import AutoTextInput from '../../common/autoTextInput';
 
 export default class HomeTwo extends Component {
   static navigationOptions = ({navigation}) => ({
@@ -9,7 +10,18 @@ export default class HomeTwo extends Component {
   });
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {text: '',
+	    data:[{key: '一'},
+						{key: '二'},
+						{key: '三'},
+						{key: '四'},
+						{key: '五'},
+						{key: '六'},
+						{key: '七'},
+						{key: '八'},
+						{key: '九'},
+						{key: '十'},
+					]};
   }
   
   componentDidMount() {
@@ -29,9 +41,11 @@ export default class HomeTwo extends Component {
   }
   _sendMessage(msg){
   	ws.send(msg);
+  	this.state.data.push("111");
+  	console.log(this.data);
   }
-//ImagePicker = require('react-native-image-picker');
-  _getImg(){
+
+  _selectImageFile(){
   	options = {
 		  title: 'Select Avatar',
 		  customButtons: [
@@ -59,36 +73,103 @@ export default class HomeTwo extends Component {
 		
 		    // You can also display the image using data:
 		    // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-		
+				console.log(source);
+				this._sendImage(response.uri);
 		    this.setState({
 		      avatarSource: source
 		    });
 		  }
 		});
   }
+  _sendImage(imageuri){
+    let formData = new FormData();
+    let imgFile = {uri: imageuri,type:'multipart/form-data',name:'image.png'};
+    console.log(formData);
+    formData.append('imgFile',imgFile);
+		//  http://119.23.209.74:8080/upload
+    fetch('http://192.168.56.1:8080/upload',{
+      method:'POST',
+      headers:{
+        'Content-Type':'multipart/form-data',
+      },
+      body:formData,
+    })
+    .then((response)=>response.text())
+    .then((responseData)=>{
+      console.log('responseData',responseData);
+    })
+    .catch((error)=>{console.error('error',error)});
+  }
+ 
   render() {
-	  const {params} = this.props.navigation.state; 
+	  const {params} = this.props.navigation.state;
     return (
-      <View>
-        <Text>我就是--{params.val}</Text>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text}
-        </Text>
-        <Button
-				  onPress={() => this._sendMessage(this.state.text)}
-				  title="发送"
-				/>
-				<Button
-				  onPress={() => this._getImg()}
-				  title="图片"
-				/>
-				<Image source={this.state.avatarSource} style={{width:100,height:100}} />
+      <View style={styles.container}>
+      	<View  style={styles.msgContainer}>
+      		<FlatList
+					  data={this.state.data}
+					  extraData={this.state}
+					  renderItem={ ({item}) => 
+					  	<View>
+					  		<View style={{backgroundColor:"bisque",flexDirection:"row",justifyContent:"flex-start"}}>
+					  			<Image source={require('../../res/images/logo.png')} style={styles.msgAvatar}/>
+							  	<Text style={{padding:10,marginRight:76}}>我就是--{item.key} {this.state.text}</Text>
+							  	<Image source={this.state.avatarSource} style={{width:100,height:100}} />
+						  	</View>
+						  	<View style={{backgroundColor:"brown",flexDirection:"row",justifyContent:"flex-end"}}>
+						  		<Image source={this.state.avatarSource} style={{width:100,height:100}} />
+							  	<Text style={{padding:10,marginLeft:76}}>我就是--{params.val} {this.state.text}</Text>
+							  	<Image source={require('../../res/images/logo.png')} style={styles.msgAvatar}/>
+						  	</View>
+					  	</View>
+					  }
+				  />
+      	</View>
+      	<View style={styles.msgInputContainer}>
+          <AutoTextInput
+          	style={{flex:1}}
+	          placeholder="Type here to translate!"
+	          onChangeText={(text) => this.setState({text})}
+	        />
+			    <View style={{width:60,height:40,margin:3}}>
+			    	<Button
+			    		onPress={() => this._sendMessage(this.state.text)}
+			    		title="发送"/>
+			    </View>
+			    <View style={{width:40,height:40,margin:3}}>
+			    	<Button
+			    		onPress={() => this._selectImageFile()}
+			    		title="+"/>
+			    </View>
+      	</View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: "space-between",
+    backgroundColor:"red"
+  },
+  msgContainer: {
+	  flex:1,
+  	backgroundColor:"#eee"
+  },
+  msgAvatar:{
+  	width:28,
+  	height:28,
+  	borderRadius:28,
+  	margin:5
+  },
+  msgInputContainer: {
+  	backgroundColor: "bisque",
+//  flex:1,
+ 		flexDirection: 'row',
+ 		justifyContent: "space-between",
+ 		alignItems: 'flex-end',
+// 	height:48
+  },
+})
