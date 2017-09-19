306d80d3cb5a8ec2ab17d96ef5cbf9ca49e9dbd9
pengchen <2601349541@qq.com>
2017-09-19 18:00:48 +0800

    home

--- app/ui/home/subPage/finished.js
+++ app/ui/home/subPage/finished.js
@@ -13,7 +13,7 @@ export default class editInfo extends Component {
 				  />
 	      <FlatList
 					  data={[
-					    {key: '一'},
+					    {key: '一The easiest way to get started with React is to use this Hello World example code on CodePen. You dont need to install anything; you can just open it in another tab and follow along as we go through examples. If youd rather use a local development environment, check out the Installation page'},
 							{key: '二'},
 							{key: '三'},
 							{key: '四'},
@@ -37,17 +37,31 @@ export default class editInfo extends Component {
   }
 	_renderItem = ({item}) => (
 						<View>
-							<View style={{backgroundColor:'bisque',padding:5,marginTop:10}}>
+							<View style={{backgroundColor:'bisque',padding:8,marginTop:10}}>
 								<View style={{backgroundColor:'#fff',flex:1,flexDirection:'row'}}>
-									<View style={{flex:1,backgroundColor:"brown",width:30,height:30}}>
-										<Image source={require('../../../res/images/logo.png')} style={{width:30,height:30,borderRadius:30}}/>
+									<View style={{backgroundColor:"brown",width:48,height:48}}>
+										<Image source={require('../../../res/images/logo.png')} style={{width:48,height:48,borderRadius:48}}/>
 									</View>
-									<View>
-										<Text>aaa</Text>
+									<View style={{flex:1,flexDirection:'row'}}>
+										<View style={{flex:1,justifyContent:'center',paddingLeft:10}}>
+											<Text>FlyingWWS</Text>
+											<Text>2017-09-20</Text>
+										</View>
+										<View style={{justifyContent:'center',backgroundColor:"brown"}}>
+											<Text>? 问答</Text>
+										</View>
 									</View>
 								</View>
-								<View style={{backgroundColor:'#eee'}}>
+								<View style={{backgroundColor:'#fff'}}>
 									<Text>{item.key}</Text>
+								</View>
+								<View style={{flex:1,flexDirection:'row',backgroundColor:'#eee'}}>
+									<View style={{flex:1}}>
+										<Text> $ 1元</Text>
+									</View>
+									<View>
+										<Text>抢单 </Text>
+									</View>
 								</View>
 							</View>
 				  	</View>
